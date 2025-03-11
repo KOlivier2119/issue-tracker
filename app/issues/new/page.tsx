39860caded9e07e08af6e87f09afdfb3b35b18e4
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2025-03-11 19:15:34 +0600

    Customize radix ui theme.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,4 +10,4 @@ const NewIssuePage = () => {
   );
 };
 
-export default NewIssuePage;
+export default NewIssuePage; 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import "./theme-config.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter'
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,13 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="iris">        
           <Navbar />
           <main className="p-5    ">
             {children}
-          </main>
+          </main>           
         </Theme>
       </body>
     </html>
