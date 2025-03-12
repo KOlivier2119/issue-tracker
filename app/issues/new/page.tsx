bdb993ae9e66b96643d0edb5ba31a3723a9d241d
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2025-03-13 01:11:34 +0600

    using axios and redirecting user back to the issues page.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,6 +4,8 @@ import { Button, TextField } from '@radix-ui/themes';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 interface IssueForm {
     title: string;
@@ -11,10 +13,14 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+    const router = useRouter();
     const { control, handleSubmit } = useForm<IssueForm>();
 
     return (
-        <form className="max-w-xl space-y-3" onSubmit={handleSubmit((data) => console.log(data))}>
+        <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async (data) => {
+            await axios.post('/api/issues', data);
+            router.push('/issues')
+        })}>
             {/* Use Controller for TextField */}
             <Controller
                 name="title"
